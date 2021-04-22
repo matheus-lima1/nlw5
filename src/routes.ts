802794d739e10755b6e7d0968c84be4353ce9215
Matheus Lima <matheusbmlima2020@gmail.com>
2021-04-21 21:19:01 -0300

    enviando mensagems

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { getCustomRepository } from "typeorm";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsoController";
+import { UsersController } from "./controllers/UsersCOntroller";
 
 
 const routes = Router();
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 
 routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create);
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
 
 export { routes };
